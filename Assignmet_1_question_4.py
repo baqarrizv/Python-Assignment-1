2b9cb81452d1f7d8111f17e98c94983001ee23b8
baqarrizv <baqarrizvi72@gmail.com>
2019-11-03 23:25:02 +0500

    Assignment done

---
+++ Assignmet_1_question_4.py
@@ -0,0 +1,12 @@
+
+
+radius = int(input("Enter Radius of a Cirle"))
+circle =  3.14 * radius * radius
+print(circle)
+
+
+
+
+
+
+
---
+++ Assignmet_1_question_5.py
@@ -0,0 +1,10 @@
+
+a = input("Enter your First name")
+b = input("Enter your Last name")
+print(b , a)
+
+
+
+
+
+
